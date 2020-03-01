90eb275df61557633e86d7ece98bcace4cd974c2
Shachar Langbeheim <shachar@lightricks.com>
2020-03-01 16:47:51 +0200

    add minimal documentation.

--- TediousStuff.hpp
+++ TediousStuff.hpp
@@ -5,18 +5,23 @@
 
 namespace Tedious {
 
+/// Populates the given objects with models and animations.
 void makeModels(std::vector<std::unique_ptr<OO::Virtual::Model>> &virtualModels,
                 std::vector<OO::Static::Model> &staticModels,
                 OO::StaticWithSpan::Input &staticWithSpanInput,
                 DataOriented::Input &dataOrientedInput, int numberOfModels,
                 int numberOfAnimationsPerModel);
 
+/// Verifies that all of the models across the different implementations are the
+/// same.
 void verifyInitialModels(
     const std::vector<std::unique_ptr<OO::Virtual::Model>> &virtualModels,
     const std::vector<OO::Static::Model> &staticModels,
     const std::vector<OO::StaticWithSpan::Model> &staticWithSpanModels,
     const std::vector<DataOriented::InputValue> &dataOrientedInputs);
 
+/// Verifies that all of the models across the different implementations have
+/// been interpolated in the same way.
 void verifyModels(
     const std::vector<std::unique_ptr<OO::Virtual::Model>> &virtualModels,
     const std::vector<OO::Static::Model> &staticModels,
